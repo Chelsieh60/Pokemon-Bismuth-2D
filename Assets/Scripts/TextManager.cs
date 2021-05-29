35cb398d1be2d1abc228eb05b617eab568be55d6
Chelsieh60 <79162273+Chelsieh60@users.noreply.github.com>
2021-05-29 13:25:48 -0500

    scripts
    Adding more scripts

---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    [SerializeField] GameObject TextBox;
+    [SerializeField] Text Text;
+
+    public void ShowText()
+    {
+
+    }
+}
