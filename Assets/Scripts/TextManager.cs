7a771685f133d3d8e3ff91d03fe0b53b6fbb8f55
Chelsieh60 <79162273+Chelsieh60@users.noreply.github.com>
2021-05-29 15:34:03 -0500

    Pokemon
    Added Pokemons objects

--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -6,10 +6,20 @@ using UnityEngine.UI;
 public class TextManager : MonoBehaviour
 {
     [SerializeField] GameObject TextBox;
-    [SerializeField] Text Text;
+    [SerializeField] Text Texting;
 
-    public void ShowText()
+    public void ShowText(Textman textman)
     {
-
+        TextBox.SetActive(true);
+        StartCoroutine(TypeText(textman.lines[0]));
+    }
+    public IEnumerator TypeText(string enterText)
+    {
+        Texting.text = "";
+        foreach (var letter in enterText.ToCharArray())
+        {
+            Texting.text += letter;
+            yield return new WaitForSeconds(1 / 5);
+        }
     }
 }
---
+++ Assets/Scripts/Textman.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Textman
+{
+    [SerializeField] List<string> Lines;
+
+    public List<string> lines
+    {
+        get { return Lines; }
+    }
+}
